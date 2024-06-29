4bf43054ce8c57dbdccf82a7460cfae8bf010f5c
reidmen <r.rethmawn@gmail.com>
2024-06-29 19:38:19 +0200

    fixing configuration for snippets

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -1,22 +1,21 @@
 return { -- Autocompletion
   "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
   dependencies = {
-    "saadparwaiz1/cmp_luasnip",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
     "L3MON4D3/LuaSnip",
+    -- "saadparwaiz1/cmp_luasnip",
   },
   config = function()
     -- See `:help cmp`
     local cmp = require("cmp")
     local luasnip = require("luasnip")
     luasnip.config.setup()
+
+    require("luasnip.loaders.from_vscode").lazy_load()
     cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
       completion = { completeopt = "menu,menuone,noinsert" },
       -- `:help ins-completion`
       mapping = cmp.mapping.preset.insert({
@@ -27,7 +26,13 @@ return { -- Autocompletion
       sources = {
         { name = "nvim_lsp" },
         { name = "luasnip" },
+        { name = "buffer" },
         { name = "path" },
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
       },
     })
   end,
