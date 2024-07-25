f4e6340913a1f131cc1705f660ad1d473d0680c5
reidmen <r.rethmawn@gmail.com>
2024-07-25 23:23:39 +0200

    added keymaps for gitsigns

--- nvim/lua/signs.lua
+++ nvim/lua/signs.lua
@@ -1,13 +1,8 @@
 return {
   "lewis6991/gitsigns.nvim",
-  opts = {
-    signs = {
-      add = { text = "┃" },
-      change = { text = "┃" },
-      delete = { text = "_" },
-      topdelete = { text = "‾" },
-      changedelete = { text = "~" },
-      untracked = { text = "┆" },
-    },
-  },
+  config = function()
+    require("gitsigns").setup()
+    vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", { desc = "[G]it [P]review" })
+    vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "[G]git [B]lame" })
+  end,
 }
